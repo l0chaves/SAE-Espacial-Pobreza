69938ce86c8fd4eb21e10573868aea68481684b7
l0chaves <102774184+l0chaves@users.noreply.github.com>
2025-02-17 23:00:25 -0500

    Choco

--- Consolidacion base de datos.R
+++ Consolidacion base de datos.R
@@ -111,12 +111,12 @@ tasa_bpn_ant <- left_join(peso_ant, num_bpn_ant, by = c("COD_MUNIC")) %>%
 
 
 ### consolidación ----
-ANT <- cod_mun_ant %>% 
+ANT <- pob_ant %>% 
   left_join(tasa_nac_ant, by = c("COD_MUNIC")) %>%
   left_join(tasa_def_ant, by = c("COD_MUNIC")) %>%
   left_join(tasa_vio_ant, by = c("COD_MUNIC")) %>%
   left_join(tasa_bpn_ant, by = c("COD_MUNIC")) %>%
-  select(COD_MUNIC, MUNICIPIO = MUNICIPIO.y, tasa_nat, tasa_def, tasa_vio, tasa_bps, tot = pop.x)
+  select(COD_DPTO, COD_MUNIC, MUNICIPIO = MUNICIPIO.y, tasa_nat, tasa_def, tasa_vio, tasa_bps, tot = pop.x)
 
 #------------------------------------------------------------------------------#
 
@@ -207,15 +207,112 @@ tasa_bpn_atl <- left_join(peso_atl, num_bpn_atl, by = c("COD_MUNIC")) %>%
 
 
 ### consolidación ----
-ATL <- cod_mun_atl %>% 
+ATL <- pob_atl %>% 
   left_join(tasa_nac_atl, by = c("COD_MUNIC")) %>%
   left_join(tasa_def_atl, by = c("COD_MUNIC")) %>%
   left_join(tasa_vio_atl, by = c("COD_MUNIC")) %>%
   left_join(tasa_bpn_atl, by = c("COD_MUNIC")) %>%
-  select(COD_MUNIC, MUNICIPIO, tasa_nat, tasa_def, tasa_vio, tasa_bps, tot = pop.x)
+  select(COD_DPTO, COD_MUNIC, MUNICIPIO, tasa_nat, tasa_def, tasa_vio, tasa_bps, tot = pop.x)
+
+# Choco ----
+H27 <- unique(HOGARES[HOGARES$dpto == "27", c("mes","clase","dominio","lp")])
+
+cod_mun_chc <- unique(divipola[divipola$`Código Departamento` == "27", c("Código Municipio", "Nombre Municipio")])
+names(cod_mun_chc) <- c("COD_MUNIC", "MUNICIPIO")
+cod_mun_chc$COD_MUNIC <- substr(cod_mun_chc$COD_MUNIC, 3, nchar(cod_mun_chc$COD_MUNIC))
+cod_mun_chc <- na.omit(cod_mun_chc)
+cod_mun_chc <- within(cod_mun_chc,{
+  MUNICIPIO <- str_to_upper(MUNICIPIO)
+  MUNICIPIO <- chartr("ÁÉÍÓÚÜ", "AEIOUU", MUNICIPIO)
+})
+
+pob_chc <- poblacion[poblacion$DP == "27" & poblacion$AÑO == 2022 & poblacion$`ÁREA GEOGRÁFICA` == "Total", -c(2,5,6)]
+names(pob_chc) <- c("COD_DPTO", "COD_MUNIC", "MUNICIPIO","Población")
+pob_chc$COD_MUNIC <- substr(pob_chc$COD_MUNIC, 3, nchar(pob_chc$COD_MUNIC))
+pob_chc <- within(pob_chc,{
+  MUNICIPIO <- str_to_upper(MUNICIPIO)
+  MUNICIPIO <- chartr("ÁÉÍÓÚÜ", "AEIOUU", MUNICIPIO)
+})
+
+## Natalidad -------------------------------------------------------------------
+nac_chc <- nac2022[nac2022$COD_DPTO == "27", "COD_MUNIC"]
+
+# total de nacimientos
+num_nac_chc <- as.data.frame(table(nac_chc)); num_nac_chc
+num_nac_chc <- left_join(num_nac_chc, cod_mun_chc, by = c("COD_MUNIC"))
+
+tasa_nac_chc <- left_join(pob_chc, num_nac_chc, by = c("COD_MUNIC")) %>%
+  select(COD_MUNIC, MUNICIPIO = MUNICIPIO.x, Freq, pop = Población) %>%
+  mutate(tasa_nat = (Freq/pop)*1000) %>%
+  mutate(tasa_nat = replace_na(tasa_nat, 0))
+
+## Mortalidad infantil  --------------------------------------------------------
+def_chc <- nofetal2022[(nofetal2022$COD_DPTO == "27" & nofetal2022$GRU_ED2 == "01"),
+                       "COD_MUNIC"]
+
+# Total del muertes menor a 1 año
+num_def_chc <- as.data.frame(table(def_chc)); num_def_chc
+num_def_chc <- left_join(num_def_chc, cod_mun_chc, by = c("COD_MUNIC"))
+
+tasa_def_chc <- left_join(num_nac_chc, num_def_chc, by = c("COD_MUNIC")) %>%
+  select(COD_MUNIC, MUNICIPIO = MUNICIPIO.x, def = Freq.y, nac = Freq.x) %>%
+  mutate(tasa_def = (def/nac)*1000) %>%
+  mutate(tasa_def = replace_na(tasa_def, 0))
+
+## Violencia Intrafamiliar ----------------------------------------------------- 
+vio_chc <- vio2022[vio2022$DEPARTAMENTO == "CHOCÓ", c("MUNICIPIO", "CANTIDAD")]
+num_vio_chc <- vio_chc %>%
+  group_by(MUNICIPIO) %>%
+  summarise(Freq = sum(CANTIDAD)) %>%
+  mutate(MUNICIPIO = ifelse(MUNICIPIO == "Quibdó (CT)", "QUIBDO", MUNICIPIO))
+
+num_vio_chc <- na.omit(num_vio_chc)
+num_vio_chc <- within(num_vio_chc,{
+  MUNICIPIO <- str_to_upper(MUNICIPIO)
+  MUNICIPIO <- chartr("ÁÉÍÓÚÜ", "AEIOUU", MUNICIPIO)
+  MUNICIPIO <- str_replace_all(MUNICIPIO, "\n", "")
+})
+
+tasa_vio_chc <- left_join(pob_chc, num_vio_chc, by = c("MUNICIPIO")) %>%
+  select(COD_MUNIC, MUNICIPIO = MUNICIPIO, Freq, pop = Población) %>%
+  mutate(tasa_vio = (Freq/pop)*100000) %>%
+  mutate(tasa_vio = replace_na(tasa_vio, 0))
+
+
+## Bajo Peso -------------------------------------------------------------------
+
+# Total de bajo peso al nacer
+bpn_chc <- nac2022[nac2022$COD_DPTO == "27"  & nac2022$PESO_NAC <= 4, "COD_MUNIC"]
+
+num_bpn_chc <- as.data.frame(table(bpn_chc)); num_bpn_chc
+num_bpn_chc <- left_join(num_bpn_chc, cod_mun_chc, by = c("COD_MUNIC"))
+
+# Total de peso registrado al nacer
+peso_chc <- nac2022[nac2022$COD_DPTO == "27"  & nac2022$PESO_NAC != 9, "COD_MUNIC"]
+peso_chc <- as.data.frame(table(peso_chc)); peso_chc
+peso_chc <- left_join(peso_chc, cod_mun_chc, by = c("COD_MUNIC"))
+
+tasa_bpn_chc <- left_join(peso_chc, num_bpn_chc, by = c("COD_MUNIC")) %>%
+  select(COD_MUNIC, MUNICIPIO = MUNICIPIO.x, Bajo = Freq.y, Tot = Freq.x) %>%
+  mutate(tasa_bps = (Bajo/Tot)*1000)  %>%
+  mutate(tasa_bps = replace_na(tasa_bps, 0))
+
+
+### consolidación ----
+CHC <- pob_chc %>% 
+  left_join(tasa_nac_chc, by = c("COD_MUNIC")) %>%
+  left_join(tasa_def_chc, by = c("COD_MUNIC")) %>%
+  left_join(tasa_vio_chc, by = c("COD_MUNIC")) %>%
+  left_join(tasa_bpn_chc, by = c("COD_MUNIC")) %>%
+  select(COD_DPTO, COD_MUNIC, MUNICIPIO, tasa_nat, tasa_def, tasa_vio, tasa_bps, tot = pop.x)
+
 
 # Guardado ----
-save(ATL, ANT, file = "Base_de_Datos.RData")
+save(ATL, ANT, CHC, file = "Base_de_Datos.RData")
+
+write.csv(ANT, file = "ANT.csv")
+write.csv(ATL, file = "ATL.csv")
+write.csv(CHC, file = "CHC.csv")
 
 
 rm(list = ls()); load("Base_de_Datos.RData")
