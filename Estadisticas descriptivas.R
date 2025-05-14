b5e9ba73d033ad76f50ede73ddb5b6fac057d432
l0chaves <102774184+l0chaves@users.noreply.github.com>
2025-05-14 13:46:29 -0500

    Summary

--- Estadisticas descriptivas.R
+++ Estadisticas descriptivas.R
@@ -1,14 +1,5 @@
 library(ggplot2)
 
-summary(ANT)
+H05 <- unique(HOGARES[HOGARES$dpto == "05", c("mes","clase","dominio","lp")])
 
-# Gráfico de barras de tasas por municipio
-ggplot(df, aes(x = MUNICIPIO, y = tasa_nat)) + 
-  geom_bar(stat = "identity", fill = "blue") +
-  labs(title = "Tasa de Natalidad por Municipio", x = "Municipio", y = "Tasa de Natalidad") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
-ggplot(df, aes(x = MUNICIPIO, y = tasa_def)) + 
-  geom_bar(stat = "identity", fill = "green") +
-  labs(title = "Tasa de Defunciones por Municipio", x = "Municipio", y = "Tasa de Defunciones") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
